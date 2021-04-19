26cb605b76a192b2fd2f0977505d6c1b627c2cf5
builtbyjosh <pc.refuge@gmail.com>
2021-04-18 23:23:26 -0500

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -19,7 +19,7 @@ class GifListContainer extends Component {
   }
 
   fetchGIFs = (query = "cigar") => {
-    fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g&limit=3`)
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=iMeMaexHPb2d56ESEOiPv8PNhy3tXufm&rating=g&limit=3`)
       .then(res => res.json())
       .then(({data}) => {
         this.setState({ gifs: data.map( gif => ({ url: gif.images.original.url }) ) })
